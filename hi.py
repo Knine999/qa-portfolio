5073f920a41e36d23bf1e63a98683e95d051399d
Iván Romero <ivan528491@gmail.com>
2026-09-22 00:21:37 -0500

    New changes

--- hi.py
+++ hi.py
@@ -5,3 +5,7 @@ print(type(name))
 print(type(age))
 print(name + " tiene " + str(age) + " años") #Convertir un int a string para concatenar
 print(f"{name} tiene {age} años") #Usando f-strings (más moderno y eficiente) para evitar concatenar y convertir tipos de datos
+
+lenguaje = "Python"
+semanas = 30
+print(f"Voy a aprender {lenguaje} en {semanas} semanas")
